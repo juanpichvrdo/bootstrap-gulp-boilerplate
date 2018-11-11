de97a25d0ab80a69b63ed8e226c36f4b57e6892f
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-11-11 17:37:53 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  return gulp
+    .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task("js", function() {
+  return gulp
+    .src([
+      "node_modules/bootstrap/dist/js/bootstrap.min.js",
+      "node_modules/jquery/dist/jquery.min.js",
+      "node_modules/popper.js/dist/umd/popper.min.js"
+    ])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task("serve", ["sass"], function() {
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch(
+    ["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"],
+    ["sass"]
+  );
+  gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", ["js", "serve"]);
